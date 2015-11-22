fc4fdcd7ea28dce09c6dcd462c81bc5efc479b6f
Simon Cadman <src@niftiestsoftware.com>
2015-11-22 15:37:09 +0000

    Fix deleting, delete after 7 days

--- cron.py
+++ cron.py
@@ -15,8 +15,7 @@ dbconn = sqlite3.connect('log.db')
 c = dbconn.cursor()
 
 now = datetime.datetime.now()
-then = now - datetime.timedelta(days=14)
+then = now - datetime.timedelta(days=7)
 timestamp = then.strftime('%s')
-
-c.execute("delete from addresses where timestamp <= ?", ( timestamp ))
+c.execute("delete from addresses where timestamp <= ?", (timestamp,) )
 dbconn.commit()
